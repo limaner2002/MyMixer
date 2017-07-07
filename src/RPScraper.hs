35ade2998916849f5b20528a3b33a6764dd4e60e
Joshua McCartney <joshuamccartney@iMac.lan>
2017-07-06 20:23:11 -0400

    Now uses the Applicative instance of the Fold to construct the Tracks in a safer way.

--- src/RPScraper.hs
+++ src/RPScraper.hs
@@ -10,7 +10,7 @@ import Control.Arrow
 import Database.Persist.Sqlite (runSqlite, SqlBackend)
 import qualified Streaming.Prelude as S
 import Text.Taggy.Lens
-import Control.Lens hiding (children)
+import Control.Lens hiding (children, elements)
 import Data.List.Split (chunksOf)
 import qualified Data.Text.Lazy as TL
 import Data.ByteString.Streaming.HTTP hiding (withHTTP)
@@ -23,26 +23,26 @@ rpScraper dbLocation = runSqlite dbLocation rpScraper'
 rpScraper' :: (MonadIO m, MonadBaseControl IO m) => ReaderT SqlBackend m ()
 rpScraper' = do
   tracks <- liftIO $ getTrackInfo
-  makeTracks >>> S.each >>> S.concat >>> addTracks $ tracks
+  parseTrackInfo >>> S.each >>> addTracks $ tracks
 
 addTracks :: MonadIO m =>
      S.Stream (S.Of Track) (ReaderT SqlBackend m) r
      -> ReaderT SqlBackend m r
 addTracks = S.mapM_ (flip addToDB (StationKey 1000))
 
-createTrack :: [Text] -> Either String Track
-createTrack [artist, name, album] = Right $ Track artist name (Just album) Nothing
-createTrack l = Left "Incorrect input"
-
-createTracks = fmap createTrack . chunksOf 3
-
 rpURL = "http://www.radioparadise.com/rp2-content.php?name=Playlist&more=true"
 
 getTrackInfo :: IO TL.Text
 getTrackInfo = decodeUtf8 . responseBody <$> (join $ httpLbs <$> parseRequest rpURL <*> newManager tlsManagerSettings)
 
-parseTrackInfo :: TL.Text -> [[Text]]
-parseTrackInfo x = chunksOf 4 $ x ^.. html . taking 1 (dropping 7 $ allNamed (only "table")) . allNamed (only "a") . children . folded . runFold (Fold content <|> Fold contents)
+parseTrackInfo :: TL.Text -> [Track]
+parseTrackInfo x = x ^.. html . taking 1 (dropping 7 $ allNamed (only "table")) . allNamed (only "tr") . runFold (Track <$> getArtist <*> getTrack <*> (Just <$> getAlbum) <*> pure Nothing)
 
-makeTracks :: TL.Text -> [Either String Track]
-makeTracks = parseTrackInfo >>> fmap (take 3) >>> fmap createTrack
+getArtist :: HasElements s => ReifiedFold s Text
+getArtist = Fold (elements . elements . contents)
+
+getTrack :: HasElements s => ReifiedFold s Text
+getTrack = Fold (elements . elements . elements . contents)
+
+getAlbum :: HasElements s => ReifiedFold s Text
+getAlbum = Fold (taking 1 $ elements . elements . elements . elements . elements . elements . contents)
