bf530797dada85338122b9d711f7955bc10bab09
Joshua McCartney <joshuamccartney@iMac.lan>
2018-01-01 17:09:18 -0500

    Added missing McScraper.hs

---
+++ src/McScraper.hs
@@ -0,0 +1,114 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module McScraper
+    ( mcScraper
+    ) where
+
+import ClassyPrelude
+import Prelude (iterate)
+import Control.Monad (forM_)
+import Control.Monad.IO.Class (liftIO)
+import Transient.Base
+import Transient.Indeterminism
+import Transient.EVars
+import Control.Applicative ((<|>))
+import System.Random
+import Data.Conduit
+import Data.Conduit.Attoparsec
+import qualified Data.Conduit.Combinators as CC
+import Data.Aeson
+import Control.Monad.Trans.Resource
+import Network.HTTP.Conduit
+import Control.Monad.Loops
+import Control.Exception
+import Database.Persist.Sqlite (runSqlite, Key, runMigration)
+
+import Core
+
+mcScraper :: TransIO ()
+mcScraper = do
+  mgr <- liftIO $ newManager tlsManagerSettings
+  var <- newEVar
+
+  -- (track, stationId) <- foldr (<|>) empty $ fmap (getTrackStation mgr . StationKey) [44, 3]
+  scrapeStations mgr var <|> (threads 1 $ saveStationTrack var)
+
+scrapeStations :: Manager -> EVar (Track, Key Station) -> TransIO ()
+scrapeStations mgr var = foldr (<|>) empty $ fmap (getTrackStation mgr var . StationKey) [44,3,117,2,4,6,14,22,27,32,35,36,38,39,40,47,48,150,1,18,15]
+
+saveStationTrack :: EVar (Track, Key Station) -> TransIO ()
+saveStationTrack var = do
+  (track, stationId) <- readEVar var
+  liftIO $ do
+    id <- myThreadId
+    putStrLn $ pprintTrack track
+
+    runSqlite dbLocation $ addToDB track stationId
+
+getTrackStation :: Manager -> EVar (Track, Key Station) -> Key Station -> TransIO ()
+getTrackStation mgr var stationId = do
+  mTrack <- iterateEvents' (getTrack mgr (baseURL <> show (unStationKey stationId))) (0, Nothing)
+  case mTrack of
+    Just track -> writeEVar var (track, stationId)
+    Nothing -> return ()
+
+getTrack :: Manager -> String -> (Int, Maybe Track) -> IO (Maybe Track, (Int, Maybe Track))
+getTrack mgr url (delay, mOldTrack) = do
+  threadDelay (delay * 1000)
+  mVal <- worker mgr url
+  case mVal of
+    Nothing -> error $ "Could not access the url: " <> url
+    Just val ->
+        case val of
+          Left msg -> error $ unpack msg
+          Right (MCTrack tup@(track, delay)) -> do
+                   case mOldTrack of
+                     Nothing -> return (Just track, (delay, Just track))
+                     Just oldTrack ->
+                         case oldTrack == track of
+                           True -> return (Nothing, (delay, Just track))
+                           False -> return $ (Just track, (delay, Just track))
+
+mul :: Int
+mul = truncate 1e6
+
+worker :: FromJSON a => Manager -> String -> IO (Maybe (Either Text a))
+worker mgr url = do
+  request <- liftIO $ parseRequest url
+  res <- runResourceT $ httpRequest mgr request
+  return res
+
+decodeJSON :: FromJSON a => MonadResource m => ConduitM (PositionRange, Value) (Either Text a) m ()
+decodeJSON = CC.map (\(_, x) -> case fromJSON x of
+                                         Error msg -> Left $ pack msg
+                                         Success val -> Right val
+                    )
+
+httpRequest :: MonadResource m => FromJSON a => Manager -> Request -> m (Maybe (Either Text a))
+httpRequest mgr req = do
+  response <- http req mgr
+  responseBody response $$+- conduitParser json =$ decodeJSON =$ CC.last
+
+baseURL :: Textual t => t
+baseURL = "http://websiteservices.musicchoice.com/api/channels/NowPlaying/ttla/"
+
+newtype MCTrack = MCTrack (Track, Int)
+    deriving Show
+
+instance FromJSON MCTrack where
+    parseJSON (Object o) = MCTrack <$> tuple
+        where
+          track = Track <$> o .: "Line1"
+                        <*> o .: "Line2"
+                        <*> o .: "Line3"
+                        <*> pure Nothing
+          ttl = o .: "TimeToLive"
+          tuple = (,) <$> track <*> ttl
+           
+iterateEvents' :: (a -> IO (b, a)) -> a -> TransIO b
+iterateEvents' f init = go init
+    where
+      go x = do
+        (output, feedback) <- async (f x)
+        return output <** go feedback
